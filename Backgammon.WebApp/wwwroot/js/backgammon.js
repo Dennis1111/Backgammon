ee8a04e68cb1d0a2ede0ff85ea02a627f5384915
dennis <dennis.nilsson1111@gmail.com>
2025-05-19 02:11:40 +0200

    remove dead code

--- Backgammon.WebApp/wwwroot/js/backgammon.js
+++
@@ -1,7 +0,0 @@
-import { initializeGameControls } from './viewGameControls.js';
-import { updateBoard } from './updateBoard.js';
-import { updateGamePanel } from './updateGamePanel.js';
-
-export function initializeGame() {
-    initializeGameControls(updateBoard, updateGamePanel);
-}
